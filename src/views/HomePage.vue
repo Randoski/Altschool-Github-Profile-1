35e0066754bac37c22617030c93e614463812858
Randy Monye <70471739+Randoski@users.noreply.github.com>
2023-03-23 14:49:35 +0100

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,13 +2,12 @@
   <div>
     <Layout />
     <div class="mt-[3rem] max-md:mt-[10rem] hero-con">
-      <div class="flex flex-row items-center justify-between">
-
+      <div class="con1">
         <!-- Hero text -->
         <div>
           <h1 class="front-page-heading tracking-tighter">Frontend</h1>
           <h1 class="front-page-heading tracking-tighter">Developer</h1>
-          <p class="temp">{{this.bio}}</p>
+          <p class="temp">{{ this.bio }}</p>
           <router-link to="/repositories"
             ><Btn
               className="bg-slate-700 hover:bg-black"
@@ -19,7 +18,7 @@
 
         <!-- Image -->
         <div class="img-and-bg-container">
-          <img src="../assets/404.svg" alt="404" class="user-img" />
+          <img :src="this.displayImage" alt="User image" class="user-img" />
         </div>
       </div>
     </div>
@@ -45,28 +44,27 @@ export default {
   },
 
   data() {
-      return {
-        displayImage: "",
-        bio: "",
-      };
-    },
+    return {
+      displayImage: "",
+      bio: "",
+    };
+  },
 
-    methods: {
-      async github() {
+  methods: {
+    async github() {
       const response = await fetch("https://api.github.com/users/Randoski");
       const data = await response.json();
       this.displayImage = data.avatar_url;
       this.bio = data.bio;
       console.log(this.bio);
-    }
     },
+  },
 
-    mounted() {
-      this.github();
-    },
+  mounted() {
+    this.github();
+  },
 };
 </script>
-
 
 <style>
 .hero-con {
@@ -87,14 +85,17 @@ export default {
   color: white;
 }
 
-.img-and-bg-container {
-  position: relative;
-}
-
 .user-img {
-  height: 35vw;
+  height: auto;
   width: 100%;
   margin-left: 10rem;
+}
+
+.con1 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 4em;
 }
 
 @media (max-width: 801px) {
@@ -104,6 +105,16 @@ export default {
   }
   .socials {
     display: none;
+  }
+
+  .con1 {
+    flex-direction: column;
+    margin-left: 0;
+  }
+
+  .user-img {
+    margin-left: 0;
+    margin-top: 24px;
   }
 }
 </style>
